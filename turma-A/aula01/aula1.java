2c295cd136285bcbdf77a0a35c3f213febb59f35
Júlia Benedetti <133465986+benedettii@users.noreply.github.com>
2023-05-13 14:23:39 +0000

    aula1

---
+++ turma-A/aula01/aula1.java
@@ -0,0 +1,82 @@
+    import java.util.Scanner;
+    public class aula1 {
+    public static void main(String[] args) {
+
+//         double abacaxi = 7.30;
+//         double maca = 2.0;
+//         double pera = 3.0;
+
+//         Scanner conta = new Scanner(System.in);
+
+//         System.out.println ("Digite a quantidade de abacaxi que você deseja:");
+//         abacaxi *= conta.nextInt();
+
+//         System.out.println ("Digite a quantidade de maca que você deseja:");
+//         maca *= conta.nextInt();
+
+//         System.out.println ("Digite a quantidade de pera que você deseja:");
+//         pera *= conta.nextInt();
+
+//         double precototal = abacaxi + maca + pera;
+//         System.out.println ("O preço total é " + precototal);
+// ----------------------------------------------------------------------------------------------
+// //                                  ex 2;
+//         Scanner verificacao = new Scanner (System.in);
+
+//         System.out.println ("Digite o primeiro valor:");
+//         int valor1 = verificacao.nextInt();
+
+//         System.out.println ("Digite o primeiro valor:");
+//         int valor2 = verificacao.nextInt();
+
+//         if (valor1 == valor2) {
+//         System.out.println ("Os valores são iguais");
+//         } else if (valor1 > valor2) {
+//             System.out.println ("O valor 1  é maior.");
+//         } else if (valor2 > valor1) {
+//             System.out.println ("O valor 2 é maior.");
+//         } else if (valor1 < valor2) {
+//             System.out.println ("O valor 1 é menor.");
+//         } else {
+//             System.out.println ("O valor 2 é menor.");
+//         }
+        
+
+    //                                      ex 3;
+    // -----------------------------------------------------------------------------------------------
+//         Scanner comparacao =  new Scanner (System.in);
+
+//         System.out.println ("Digite a primeira idade:");
+//         int idade1 = comparacao.nextInt();
+
+//         System.out.println ("Digite a segunda idade:");
+//         int idade2 = comparacao.nextInt();
+
+//         if (idade1 > 18) {
+//         System.out.println ("Considerado de maior.");
+//     } else if (idade2 > 18) {
+//         System.out.println("É considerado de maior.");
+//     }
+        //                                     ex 4;
+        // ------------------------------------------------------------------------------------------------
+        // Scanner idade = new Scanner (System.in);
+
+        //     System.out.println ("Digite a primeira idade:");
+        //     int idade01 = idade.nextInt();
+
+        //     System.out.println ("Digite a segunda idade:");
+        //     int idade02 = idade.nextInt();
+
+        //     if (idade01 > idade02){
+        //     System.out.println ("A idade 01 é maior que a idade 02.");
+        //     } else if (idade02 > idade01){
+        //     System.out.println ("A idade 02 é maior que a idade 01.");
+        //     } else {
+        //         System.out.println("AS idades são iguais");
+        //     }
+            //                                    ex5;
+            // ------------------------------------------------------------------------------------------------
+            //                                   ex6;
+            // ------------------------------------------------------------------------------------------------
+            Scanner booleann = new Scanner (System.in);
+            boolean guarda = booleann.next
